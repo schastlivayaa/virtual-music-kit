2fa283d9d0e34a66711ccfb2a64ca35b4b2850bf
Zlata Schastlivaya <s4astlivaya.zlata11@yandex.ru>
2025-11-03 15:39:11 +0300

    feat: add sounds data

---
+++ virtual-music-kit/index.js
@@ -0,0 +1,20 @@
+// массив нот
+const sounds = [
+    {id: 'do', label: 'Do', file: 'assets/do.mp3', code: 'KeyA'},
+    {id: 're', label: 'Re', file: 'assets/re.mp3', code: 'KeyS'},
+    {id: 'mi', label: 'Mi', file: 'assets/mi.mp3', code: 'KeyD'},
+    {id: 'fa', label: 'Fa', file: 'assets/fa.mp3', code: 'KeyF'},
+    {id: 'salt', label: 'Salt', file: 'assets/salt.mp3', code: 'KeyG'},
+    {id: 'la', label: 'La', file: 'assets/la.mp3', code: 'KeyH'},
+    {id: 'si', label: 'Si', file: 'assets/si.mp3', code: 'KeyJ'},
+    {id: 'do-sharp', label: 'Do#', file: 'assets/do-sharp.mp3', code: 'KeyW'},
+    {id: 're-sharp', label: 'Re#', file: 'assets/re-sharp.mp3', code: 'KeyE'},
+    {id: 'fa-sharp', label: 'Fa#', file: 'assets/fa-sharp.mp3', code: 'KeyT'},
+    {id: 'salt-sharp', label: 'Salt#', file: 'assets/salt-sharp.mp3', code: 'KeyY'},
+    {id: 'la-sharp', label: 'La#', file: 'assets/la-sharp.mp3', code: 'KeyU'},
+];
+
+// создание объекта audio для каждой ноты
+sounds.forEach(s => {
+    s.audio = new Audio(s.file)
+});
